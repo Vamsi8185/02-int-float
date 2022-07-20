d5110ee07816b2f2f5e71c80e9f3c68df8897855
Vamsi8185 <109525479+Vamsi8185@users.noreply.github.com>
2022-07-20 15:54:20 +0530

    Add files via upload

---
+++ (vamsi)int@float.py
@@ -0,0 +1,31 @@
+# a= 123
+# print(type(a))
+# b=32.098
+# print(type(b))
+# c=2.864
+# print(type(c))
+# print(type(575164554788756))
+# a=5 ;b=4
+# print('%d '%b)
+
+# e=5.365
+# print(e)
+# print('%d '%e)
+# 1=7.634
+# print('%.1f'%1)
+# print('%.2f'%1)
+# print('%.3f'%1)\
+
+
+''' Types of  Conversion & casting '''
+# m=7453
+# print(type(m))
+# print(float(m))
+# print('%d'%m)
+# print('%f'%n)
+n=6.87
+print(type(n))
+print(int(n))
+print('%d'%n)
+print('%f'%n)
+print('%f'%)
